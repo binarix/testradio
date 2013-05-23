d2f0f1cd5ff47a2b735abfd9b2fee5a37e1ffb53
Jia Miew (John) Tan <john@binarix.net>
2013-05-24 08:58:44 +1000

    initial commit

--- app/config/app.php
+++ app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => '9gZn9mQxYdsOvUKswVhKpaOKMAlc1ajG',
 
 	/*
 	|--------------------------------------------------------------------------
