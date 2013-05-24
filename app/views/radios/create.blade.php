8c2853492a1f2814f01c40c8149288871d4ed572
Jia Miew (John) Tan <john@binarix.net>
2013-05-24 13:47:33 +1000

    added instructions of the Input::get issue to views

--- app/views/radios/create.blade.php
+++ app/views/radios/create.blade.php
@@ -30,35 +30,46 @@ Input::old('station') = {{ Input::old('station') }}
     <li>Click on the link above to navigate to radios/create. View::make('radios.create'); called.</li>
     <ul>
         <li>Input::old('station') = initially empty <strong>EXPECTED</strong></li>
+        <li>No message <strong>EXPECTED</strong></li>
     </ul>
     <li>Select <strong>alpha</strong> radio button. Hit submit button. Redirect::route('radios.create')->withInput(); called.</li>
     <ul>
         <li>Input::old('station') = alpha <strong>EXPECTED</strong></li>
+        <li>Message is Input::get('station') from store method = alpha <strong>EXPECTED</strong></li>
     </ul>
     <li>Select <strong>beta</strong> radio button. Hit submit button. Redirect::route('radios.create')->withInput(); called.</li>
     <ul>
         <li>Input::old('station') = alpha <strong>UNEXPECTED</strong> Value should be beta not alpha. Why is this value not updated to beta?</li>
+        <li>Message is Input::get('station') from store method = alpha <strong>UNEXPECTED</strong> Value should be beta not alpha.</li>
     </ul>
     <li>Select <strong>charlie</strong> radio button. Hit submit button. Redirect::route('radios.create')->withInput(); called.</li>
     <ul>
         <li>Input::old('station') = alpha <strong>UNEXPECTED</strong> Value should be charlie not alpha. Value still not updated to charlie</li>
+        <li>Message is Input::get('station') from store method = alpha <strong>UNEXPECTED</strong> Value should be charlie not alpha.</li>
     </ul>
     <li>Hit refresh on the browser</li>
     <ul>
         <li>Input::old('station') = empty once again <strong>EXPECTED</strong></li>
+        <li>No message <strong>EXPECTED</strong></li>
     </ul>
     <li>Select <strong>charlie</strong> radio button. Hit submit button. Redirect::route('radios.create')->withInput(); called.</li>
     <ul>
         <li>Input::old('station') = charlie <strong>EXPECTED</strong> Refreshing seems to clear the stored session old value. Now it updates to charlie</li>
+        <li>Message is Input::get('station') from store method = charlie <strong>EXPECTED</strong></li>
     </ul>
     <li>Select <strong>alpha</strong> radio button. Hit submit button. Redirect::route('radios.create')->withInput(); called.</li>
     <ul>
         <li>Input::old('station') = charlie <strong>UNEXPECTED</strong> Broken again, value should be alpha not charlie</li>
+        <li>Message is Input::get('station') from store method = charlie <strong>UNEXPECTED</strong> Value should be alpha not charlie</li>
     </ul>
 </ol>
 
 <p>
 I tried to var_dump($input) inside withInput method under Illuminate\Http\RedirectResponse.php and the value is the correct value everytime.
+</p>
+
+<p>
+As can be seen, this issue affects subsequent post submissions after the initial Redirect::route()->withInput(). The value received by the store method of the controller from the post submission of the form is also incorrect. My use case was redirecting after failed validation in my actual app but is not important for this sample as the issues lies with the Redirect::route()->withInput().
 </p>
 
 <p>
